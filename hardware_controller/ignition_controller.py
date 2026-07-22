507d74aa48e462aaa6ed59ccf34719654a385c2c
bharanivelan <bharani01072005@gmail.com>
2026-07-23 00:36:57 +0530

    fix: revert to 192.168.1.4 and apply --disable-gpu sledgehammer to Chromium

--- hardware_controller/ignition_controller.py
+++ hardware_controller/ignition_controller.py
@@ -46,7 +46,7 @@ class IgnitionController:
             f"export WAYLAND_DISPLAY=wayland-1; "
             f"export XDG_RUNTIME_DIR=/run/user/1000; "
             f"export XAUTHORITY={home}/.Xauthority; "
-            f"chromium --kiosk --disable-gpu-compositing --ozone-platform=wayland --use-gl=egl --disable-accelerated-video-decode --disable-features=WebRtcHWDecoding --disable-web-security --allow-file-access-from-files --password-store=basic file://{home}/hmi-dashboard/ignition.html > /dev/null 2>&1 &"
+            f"chromium --kiosk --disable-gpu --disable-software-rasterizer --disable-gpu-compositing --ozone-platform=wayland --use-gl=egl --disable-accelerated-video-decode --disable-features=WebRtcHWDecoding --disable-web-security --allow-file-access-from-files --password-store=basic file://{home}/hmi-dashboard/ignition.html > /dev/null 2>&1 &"
         )
         os.system(cmd)
 
